78a04c2ec8a4855d318642b59d7f7a4c5d0d7752
AbbyLiu <AbbyLiu@ABBYLIU4CCF>
2017-08-17 10:03:15 +0800

    加入專案檔案。

---
+++ EZBoss/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EZBoss.Startup))]
+namespace EZBoss
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
